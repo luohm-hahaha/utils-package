ed70491a01a11dce4d8f6930ca8d20bf2ab6503f
luohm <316043762@qq.com>
2025-08-21 16:29:51 +0800

    feat：项目初始化

---
+++ src/main/java/com/lhm/Main.java
@@ -0,0 +1,13 @@
+package com.lhm;
+
+/**
+ * @Auther: luohm
+ * @Date: 2025/8/21 - 16:26
+ * @Description: com.lhm
+ * @version: 1.0
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
